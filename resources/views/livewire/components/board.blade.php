9e546136972e2244ee2503d499e0557f98ea7896
Lauro Guedes <leowgweb@gmail.com>
2024-10-08 22:14:50 +0100

    feat(component): Create game turn attribute to control who play

--- resources/views/livewire/components/board.blade.php
+++ resources/views/livewire/components/board.blade.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Events\MoveMade;
-use Illuminate\Support\Facades\Broadcast;
 use Illuminate\Support\Facades\Cache;
 use Livewire\Attributes\On;
 use Livewire\Volt\Component;
@@ -13,10 +12,13 @@ new class extends Component {
 
     public array $board;
 
+    public bool $active;
+
     public function mount(string $gameKey): void
     {
         $this->gameId = Cache::get($gameKey);
         $this->player = session('player');
+        $this->active = $gameKey == $this->player['id'];
         $this->board = Cache::rememberForever("board_{$this->gameId}", fn() => [
             [null, null, null],
             [null, null, null],
@@ -42,18 +44,26 @@ new class extends Component {
     {
         $this->board[$payload['position']['y']][$payload['position']['x']] = $payload['player']['symbol'];
         Cache::forever("board_{$this->gameId}", $this->board);
+        $this->active = !$this->active;
     }
 }; ?>
 
-<div x-data="{
-        localBoard: $wire.entangle('board'),
-    }" class="grid grid-cols-3 gap-2 bg-white p-4 rounded-lg shadow-lg">
-    @foreach($this->board as $y => $row)
-        @foreach($row as $x => $col)
-            <div x-on:click="$wire.makeMove({{$y}}, {{$x}})"
-                 class="w-24 h-24 bg-gray-200 flex justify-center items-center text-4xl font-bold cursor-pointer hover:bg-gray-300 transition-colors duration-300">
-                <span x-text="localBoard[{{$y}}][{{$x}}]"></span>
-            </div>
+<div>
+    <div class="text-center m-3">
+        @if($active)
+            <span>You Turn</span>
+        @else
+            <span>Waiting for opponent</span>
+        @endif
+    </div>
+    <div class="grid grid-cols-3 gap-2 bg-white p-4 rounded-lg shadow-lg">
+        @foreach($this->board as $y => $row)
+            @foreach($row as $x => $col)
+                <button :disabled="!$wire.active" wire:click="makeMove({{$y}}, {{$x}})"
+                        class="w-24 h-24 bg-gray-200 disabled:cursor-not-allowed flex justify-center items-center text-4xl font-bold cursor-pointer hover:bg-gray-300 transition-colors duration-300 rounded">
+                    <span>{{ $this->board[$y][$x] }}</span>
+                </button>
+            @endforeach
         @endforeach
-    @endforeach
+    </div>
 </div>
